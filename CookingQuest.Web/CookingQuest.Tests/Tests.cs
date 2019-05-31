40b91f4d4d32e50fa67deeff6d004094fa4daf32
Paul <paul_grimes8@yahoo.com>
2019-05-31 15:07:16 -0500

    Test for SonarCloud coverage

---
+++ CookingQuest.Web/CookingQuest.Tests/Tests.cs
@@ -0,0 +1,48 @@
+﻿using CookingQuest.Web.Controllers;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CookingQuest.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Store()
+        {
+            // ARRANGE
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+               .Protected()
+               // Setup the PROTECTED method to mock
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>()
+               )
+               // prepare the expected response of the mocked http call
+               .ReturnsAsync(new HttpResponseMessage()
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("[{'id':1,'value':'1'}]"),
+               })
+               .Verifiable();
+            var httpClient = new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("http://test.com/"),
+            };
+            var subjectUnderTest = new StoreController(httpClient, new Web.MyConfiguration() { ServiceUrl = "http://test.com/"});
+
+            var result = subjectUnderTest.Index();
+            Assert.NotNull(result);
+        }
+
+    }
+}
